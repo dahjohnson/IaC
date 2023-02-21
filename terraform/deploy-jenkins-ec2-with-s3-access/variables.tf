4157b04e01264cd5b5f8fb0f76dad9e05f267330
dahjohnson <djohnson@devopslabs4me.com>
2023-02-21 07:33:04 -0500

    added terraform directory with deploy-jenkins-ec2-with-s3-access project

---
+++ terraform/deploy-jenkins-ec2-with-s3-access/variables.tf
@@ -0,0 +1,142 @@
+
+variable "vpc_cidr" {
+  description = "VPC cidr block"
+  type        = string
+  default     = "10.0.0.0/16" 
+}
+
+variable "vpc_tag" {
+  description = "Name tag for VPC"
+  type        = string
+  default     = "Demo-VPC" 
+}
+
+variable "subnet_tag" {
+  description = "Name tag for Subnet"
+  type        = string
+  default     = "Demo-Subnet" 
+}
+
+variable "subnet_cidr" {
+  description = "Subnet cidr block"
+  type        = string
+  default     = "10.0.0.0/24" 
+}
+
+variable "internet_gateway_tag" {
+  description = "Name tag for Internet Gateway"
+  type        = string
+  default     = "Demo-Internet-Gateway" 
+}
+
+variable "ami" {
+  description = "Jenkins EC2 machine image id"
+  type        = string
+  default     = "ami-0dfcb1ef8550277af" 
+}
+
+variable "instance_type" {
+  description = "Jenkins EC2 instance type"
+  type        = string
+  default     = "t2.micro" 
+}
+
+variable "ssh_key_name" {
+  description = "SSH key name for Jenkins EC2"
+  type        = string
+  default     = "ssh_key" 
+}
+
+variable "ec2_user_data" {
+  description = "User data shell script for Jenkins EC2"
+  type        = string
+  default     = <<EOF
+#!/bin/bash
+# Install Jenkins and Java 
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo amazon-linux-extras install -y java-openjdk11 
+sudo yum install -y jenkins
+sudo systemctl daemon-reload
+
+# Start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Firewall Rules
+if [[ $(firewall-cmd --state) = 'running' ]]; then
+    YOURPORT=8080
+    PERM="--permanent"
+    SERV="$PERM --service=jenkins"
+
+    firewall-cmd $PERM --new-service=jenkins
+    firewall-cmd $SERV --set-short="Jenkins ports"
+    firewall-cmd $SERV --set-description="Jenkins port exceptions"
+    firewall-cmd $SERV --add-port=$YOURPORT/tcp
+    firewall-cmd $PERM --add-service=jenkins
+    firewall-cmd --zone=public --add-service=http --permanent
+    firewall-cmd --reload
+fi
+EOF
+}
+
+variable "ec2_tag" {
+  description = "Name tag for Jenkins EC2"
+  type        = string
+  default     = "Jenkins-Server" 
+}
+
+variable "ec2-trust-policy" {
+  description = "sts assume role policy for EC2"
+  type        = string
+  default     = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
+        }   
+    ]
+}
+    EOF  
+}
+
+variable "ec2-s3-permissions" {
+  description = "IAM permissions for EC2 to S3"
+  type        = string
+  default     = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+  EOF
+}
+
+variable "bucket_name" {
+  description = "Name for S3 bucket"
+  type        = string
+  default     = "terraform1demo1s3bucket2023"
+}
+
